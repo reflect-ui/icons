42701577984ce95e8d9c7c8fecc321ef2d8ee58e
softmarshmallow <woojoo@softmarshmallow.com>
2021-01-27 12:25:21 +0900

    migrated from bridgedxyz/reflect/tools under icons/tools (material icons fetcher) with also ant design icons fetcher script wip

---
+++ tools/ant-design-icons-fetch/README.md
@@ -0,0 +1,3 @@
+# Ant Design Icons Fetcher
+
+> this fetches ant-design-icons on to the local machine, manipulates it as a desired data structure. this is uploaded to reflect icons' bucket for global, open access usage.
---
+++ tools/ant-design-icons-fetch/main.py
@@ -0,0 +1,27 @@
+
+
+DIST_DIR = "./dist"
+ANT_ICONS_ROOT_DIR = "../res/ant-design-icons"
+REPOSITORY_ZIP_URL = "https://codeload.github.com/ant-design/ant-design-icons/zip/master"
+
+
+def pre_warm():
+    # download zip, unzip the repository
+    print('downloading zip.. this might take a while')
+    r = requests.get(REPOSITORY_ZIP_URL)
+    filenamezip = r.headers.get('Content-Disposition').split('filename=')[-1]
+    filename = filenamezip.split('.zip')[0]
+    z = zipfile.ZipFile(io.BytesIO(r.content))
+    z.extractall("../res")
+    print(f'downloaded to ../res/{filename}')
+    rename(f"../res/{filename}", "../res/ant-design-icons")
+    print('res available under res/ant-design-icons')
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    pre_warm()
+    main()
